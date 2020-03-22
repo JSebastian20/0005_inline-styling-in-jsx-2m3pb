b17417ed98a79544889785bf14366f544310bff9
JSebastian20 <62495587+JSebastian20@users.noreply.github.com>
2020-03-22 23:53:37 +1030

    inline css types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const custStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "2px solid blue"
+};
+
+ReactDOM.render(
+  <h1 style={custStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(
+//   <h1 style={{ color: "red" }}>Hello World!</h1>,
+//   document.getElementById("root")
+// );
